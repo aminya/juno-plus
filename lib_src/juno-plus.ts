19319028ebbbc59df9be75cad5bd74349b1bf5d8
aminya <aminyahyaabadi74@gmail.com>
2020-03-12 15:34:57 -0500

    Fix toolbar positoin config

--- lib_src/juno-plus.ts
+++ lib_src/juno-plus.ts
@@ -5,6 +5,10 @@
         description: "Makes the size of Icons 21px (changing requires restart).",
         order:6
     },
+    // Toolbar Position
+    if (atom.config.get("juno-plus.ToolbarPosition")) {
+        atom.config.set("tool-bar.position", "Top")
+    }
     // IconSizes
     if (atom.config.get("juno-plus.IconSizes")) {
         atom.config.set("tool-bar.iconSize", '21px')
