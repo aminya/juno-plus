8ab47ac8601c38a4b4c3effa7e7ee76a8c39a100
aminya <aminyahyaabadi74@gmail.com>
2020-03-31 23:04:37 -0500

    add lib back

---
+++ lib/juno-plus.d.ts
@@ -0,0 +1,75 @@
+import { getToolbarCallback } from "./tool-bar";
+declare type JuliaClient = {
+    boot: () => void;
+    import: (arg0: {
+        rpc: string[];
+    }) => {
+        evalsimple: any;
+    };
+} | null;
+export declare const config: {
+    enableToolbarPlus: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    StartJuliaProcessButtons: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    layoutAdjustmentButtons: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    WeaveButtons: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    ToolbarPosition: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    IconSizes: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    ColorfulIcons: {
+        type: string;
+        default: boolean;
+        title: string;
+        description: string;
+        order: number;
+    };
+    JunoPackages: {
+        type: string;
+        default: string[];
+        items: {
+            type: string;
+        };
+        title: string;
+        description: string;
+        order: number;
+    };
+};
+export declare function consumeJuliaClient(client: JuliaClient): void;
+export declare function activate(): void;
+export declare function deactivate(): void;
+export declare function consumeToolBar(getToolBar: getToolbarCallback): void;
+export {};
